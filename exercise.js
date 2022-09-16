b4323c76435c8687a11bee008ac444ec82f3e3ee
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 17:06:48 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,16 @@
+// # Exercise 10
+
+// * Explain the difference between the double and the triple equals operator.
+
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+/*
+1- does the Implicit type coercion before comparing, since there is the "==" operator which 
+   triggers the Numeric conversion, so the "firstYearCompleted" variable will be casted to a number type which will be the "1" integer value.
+
+2- The triple equals operator compares the values without doing the Implicit type coercion.
+*/
+
+yearsCompleted == firstYearCompleted; // 1
+yearsCompleted === firstYearCompleted; // 2
