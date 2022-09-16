6f341a699e175a6869292808699f31ea5e428c51
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 17:20:48 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -6,7 +6,7 @@ const firstYearCompleted = true;
 const yearsCompleted = 1;
 
 /*
-1- does the Implicit type coercion before comparing, since there is the "==" operator which 
+1- Execute the Implicit type coercion before comparing, since there is the "==" operator which 
    triggers the Numeric conversion, so the "firstYearCompleted" variable will be casted to a number type which will be the "1" integer value.
 
 2- The triple equals operator compares the values without doing the Implicit type coercion.
